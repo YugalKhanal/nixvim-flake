2ee73ef50cf6f0d0872c8243d50c6d96a70ec1ad
Yugal Khanal <yugalkhanal5@gmail.com>
2025-11-18 17:07:48 +0000

    udpated config

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -1,34 +1,34 @@
-{
-  imports = [
-    ./aerials.nix
-    ./bufferline.nix
-    ./comment.nix
-    ./completions.nix
-    ./copilot.nix
-    ./dashboard.nix
-    ./lazygit.nix
-    ./leap.nix
-    ./lsp-config.nix
-    ./mappings.nix
-    ./none-ls.nix
-    ./nvim-tree.nix
-    ./presence.nix
-    # ./snacks.nix
-    ./telescope.nix
-    ./theme.nix
-    ./nvterm.nix
-    ./tree-sitter.nix
-    ./vim-options.nix
-    ./vim-tmux-navigator.nix
-    ./vimtex.nix
-    ./which-key.nix
-  ];
+{ config, lib, pkgs, ... }:
 
+{
   programs.nixvim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
+    
+    imports = [
+      ./aerials.nix
+      ./bufferline.nix
+      ./comment.nix
+      ./completions.nix
+      ./copilot.nix
+      ./dashboard.nix
+      ./lazygit.nix
+      ./leap.nix
+      ./lsp-config.nix
+      ./mappings.nix
+      ./none-ls.nix
+      ./nvim-tree.nix
+      ./presence.nix
+      # ./snacks.nix
+      ./telescope.nix
+      ./theme.nix
+      ./nvterm.nix
+      ./tree-sitter.nix
+      ./vim-options.nix
+      ./vim-tmux-navigator.nix
+      ./vimtex.nix
+      ./which-key.nix
+    ];
   };
-
-  programs.neovim.enable = true;
 }
